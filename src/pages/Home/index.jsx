652eb8205227a436271f38013c2cb68b36c4b347
alikhamesy <alikhamesy0@gmail.com>
2021-09-28 23:55:35 -0600

    finished homepage minus footer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import Projects from '../../components/Projects'
+import Break from '../../components/Break'
 
 import styles from './Home.module.scss'
 
@@ -28,7 +29,29 @@ const projects = [
 const Home = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.splash}>
+        <div className={styles.title}>
+          Hi I’m Ali, a Software Developer based out of Vancouver who’s
+          interested in Software Development
+        </div>
+        <div className={styles.subtitle}>
+          Previously at Microsoft & SAP maybe there are more things you want to
+          say here.
+        </div>
+      </div>
+      <Break />
+      <div className={styles.projects}>Selected Work</div>
       <Projects projects={projects} />
+      <Break />
+      <div className={styles.workTogether}>
+        <div className={styles.subject}>Want to Work Together?</div>
+        <div className={styles.message}>
+          Currently looking for software developer positions at companies who
+          want to revolutionize and innovate for tomorrow.
+        </div>
+      </div>
+      <Break />
+      {/* Footer */}
     </div>
   )
 }
